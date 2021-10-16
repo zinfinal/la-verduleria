251036827fd3229e35a1b6bce435ebeb8a0630f1
Harold Pacha <hrpacha@upt.pe>
2021-10-16 12:15:53 -0500

    relationship producto categoria

--- backend/src/api/models/producto.js
+++ backend/src/api/models/producto.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Producto.belongsTo(models.Categoria, {
+        foreignKey: 'producto_id',
+        onDelete: 'CASCADE'
+      });
     }
   };
   Producto.init({
